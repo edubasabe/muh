af6f62546ec1a48a74b978cb1bdeeb282055b93f
Edu Basabe <edubasabe1@gmail.com>
2016-11-14 13:24:52 -0500

    Owl Carousel listo

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,6 @@
-$(document).on('ready', function() {
-  console.log('Hola Mundo!');
-});
-
-
-// --
+// ----------------------------------------
 // jQuery to collapse the navbar on scroll
-// --
+// ----------------------------------------
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -14,9 +9,9 @@ $(window).scroll(function() {
     }
 });
 
-// --
+// ----------------------------------
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-// --
+// ----------------------------------
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -27,45 +22,52 @@ $(function() {
     });
 });
 
-// --
+// ---------------
 // Ventana modal
-// --
+// ---------------
 $(document).on('ready', function() {
-    setTimeout(function (){
-    $('#subModal').modal('show');
-  }, 2000);
+    setTimeout(function() {
+        // $('#subModal').modal('show');
+    }, 2000);
 });
 
 
-// --
+// ----------------
 // Owl Carousel
-// --
+// ----------------
 
 //-- Altura del Viewport
 var altoSlide = $(window).height();
 
-$('.home-slide').css({
- 'max-height': altoSlide + 'px!important',
- 'height': altoSlide + 'px'
-});
+// $('.home-slide').css({
+//     'max-height': altoSlide + 'px!important',
+//     'height': altoSlide + 'px'
+// });
 
-$(document).ready(function(){
-  $('#slider-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+$(document).ready(function() {
+    $('#slider-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+        navText: ['<', '>'],
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
         }
-    }
+    });
 });
+// -- Insertar Nav en el Stage
+$(document).on('ready', function() {
+$('.owl-nav').insertBefore('.owl-controls');
 });
+// ----------------
